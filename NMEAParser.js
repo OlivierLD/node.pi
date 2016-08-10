2129cb9e4ab65a3f0e759a9a956d7c6eba806272
Olivier LeDiouris <olediouris@medallia.com>
2016-08-10 10:49:39 -0700

    Completed the RMC parser

--- NMEAParser.js
+++ NMEAParser.js
@@ -68,7 +68,6 @@ var parseRMC = function(str) {
   if (data[2] === 'V') {
     return;
   }
-  var latStr = data[3];
   var latDeg = data[3].substring(0, 2);
   var latMin = data[3].substring(2);
   var lat = sexToDec(parseInt(latDeg), parseFloat(latMin));
@@ -76,7 +75,6 @@ var parseRMC = function(str) {
     lat = -lat;
   }
 
-  var lonStr = data[5];
   var lonDeg = data[5].substring(0, 3);
   var lonMin = data[5].substring(3);
   var lon = sexToDec(parseInt(lonDeg), parseFloat(lonMin));
@@ -84,12 +82,70 @@ var parseRMC = function(str) {
     lon = -lon;
   }
 
-  return { lat: lat, lon: lon };
+  var hours   = parseInt(data[1].substring(0, 2));
+  var minutes = parseInt(data[1].substring(2, 4));
+  var seconds = parseInt(data[1].substring(4, 6));
+
+  var day     = parseInt(data[9].substring(0, 2));
+  var month   = parseInt(data[9].substring(2, 4)) - 1;
+  var year    = parseInt(data[9].substring(4, 6)) + 2000;
+  var d = new Date(Date.UTC(year, month, day, hours, minutes, seconds, 0));
+
+  var sog = parseFloat(data[7]);
+  var cog = parseFloat(data[8]);
+  var W = parseFloat(data[10]);
+  if (data[11] === 'W') {
+    W = -W;
+  }
+
+  return { type: "RMC", epoch: d.getTime(), sog: sog, cog: cog, variation: W, pos: {lat: lat, lon: lon} };
 };
 
 var sexToDec = function(deg, min) {
-  return deg + ((min * 60 / 100) / 100);
+  return deg + ((min * 10 / 6) / 100);
 }
+
+/**
+ * Converts decimal degrees inot Deg Min.dd
+ * @param val value in decimal degrees
+ * @param ns_ew 'NS' or 'EW'
+ * @returns {string}
+ */
+var decToSex = function(val, ns_ew) {
+  var absVal = Math.abs(val);
+  var intValue = Math.floor(absVal);
+  var dec = absVal - intValue;
+  var i = intValue;
+  dec *= 60;
+  var s = i + "°" + dec.toFixed(2) + "'";
+
+  if (val < 0) {
+    s += (ns_ew === 'NS' ? 'S' : 'W');
+  } else {
+    s += (ns_ew === 'NS' ? 'N' : 'E');
+  }
+  return s;
+}
+
+
 // Made public.
 exports.validate = validate;
+exports.toDegMin = decToSex;
 exports.parseRMC = parseRMC;
+
+// Tests
+/*
+var val = sexToDec(333, 22.07);
+console.log(val);
+var ret = decToSex(val, 'EW');
+console.log(ret);
+
+var rmc = "$IIRMC,225158,A,3730.075,N,12228.854,W,,,021014,15,E,A*3C";
+console.log(rmc);
+console.log(validate(rmc));
+var parsed = parseRMC(rmc);
+console.log(parsed);
+console.log("Pos: " + decToSex(parsed.pos.lat, 'NS') + " " + decToSex(parsed.pos.lon, 'NS'));
+var date = new Date(parsed.epoch);
+console.log(date);
+*/
