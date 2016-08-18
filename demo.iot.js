f7df3c9cd278b3672599cc81158f55baf3bcf9f3
Olivier LeDiouris <olediouris@medallia.com>
2016-08-18 14:18:34 -0700

    Added IoT demo

---
+++ demo.iot.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// Need the Adafruit-IO key as parameter
+var key;
+if (process.argv.length > 2) {
+  key = process.argv[2];
+}
+if (key === undefined) {
+  console.log("Usage: node " + __filename + " [Your-Adafruit-IO-Key]");
+  process.exit();
+}
+
+var ONOFF_FEED = "onoff";
+var PREFIX = 'https://io.adafruit.com/api/feeds/';
+
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+// Get data
+var getSwitchState = function(cb) {
+    var url = PREFIX + ONOFF_FEED;
+    var args = {
+        headers: {"X-AIO-Key": key}
+    };
+    client.get(url, args, function (data, response) {
+        // parsed response body as js object
+    //  console.log("Last Value of [%s] (%s) was %s.", data.name, data.description, data.last_value);
+        // raw response
+    //  console.log(response);
+        cb(data.last_value);
+    });
+};
+
+var setSwitchState = function(state) {
+    var args = {
+        data: { "value": state },
+        headers: { "Content-Type": "application/json",
+                   "X-AIO-Key": key }
+    };
+    var url = PREFIX + ONOFF_FEED + "/data";
+    client.post(url, args, function (data, response) {
+        // parsed response body as js object
+    //  console.log(data);
+        // raw response
+        console.log(response.headers.status);
+    });
+};
+
+var previousState;
+var manageState = function(state) {
+  if (state !== previousState) {
+      console.log("State is now:" + state);
+      previousState = state;
+  }
+};
+
+var interv = setInterval(function() {
+  getSwitchState(manageState);
+}, 1000);
+
+setTimeout(function() {
+      setSwitchState('ON');
+    }, 5000);
+
+function exit() {
+    console.log("\nBye now!");
+    process.exit();
+};
+
+process.on('SIGINT', exit); // Ctrl C
+
