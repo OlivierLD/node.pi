e451172484dca5182c19b4b1d044313773e5fb7f
Olivier LeDiouris <olediouris@medallia.com>
2016-08-19 14:08:37 -0700

    Added some utilities

--- testUtils.js
+++ testUtils.js
@@ -8,3 +8,15 @@ console.log(value + " = " + utils.hexFmt(value, 4) + ", " + utils.binFmt(value, 
 var now = new Date();
 console.log("Now, formatted: " + now.format("H:i:s"));
 console.log("Now, formatted: " + now.format("l F dS, Y, H:i:s Z"));
+
+// Test IP address
+console.log("IP Test:");
+utils.getIPs(function (err, ip) {
+  if (ip !== undefined && ip !== null) {
+    for (var add in ip) {
+      console.log("Addr:" + ip[add]);
+    }
+  } else {
+    console.log("Found no IP ", err);
+  }
+}, false);
