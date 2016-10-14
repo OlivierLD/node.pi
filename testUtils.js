7acb3d6092041e18ae13bae284bd999d6cf7c823
Olivier LeDiouris <olediouris@medallia.com>
2016-10-14 12:17:15 -0700

    Small changes

--- testUtils.js
+++ testUtils.js
@@ -8,6 +8,7 @@ console.log(value + " = " + utils.hexFmt(value, 4) + ", " + utils.binFmt(value, 
 var now = new Date();
 console.log("Now, formatted: " + now.format("H:i:s"));
 console.log("Now, formatted: " + now.format("l F dS, Y, H:i:s Z"));
+console.log("Now, formatted: " + now.format("D M dS, Y, H:i:s Z"));
 
 // Test IP address
 console.log("IP Test:");
