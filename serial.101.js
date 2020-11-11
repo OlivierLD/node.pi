5b85afe340f7f2ba3f34ac112ff1714a3041a630
olivier.lediouris <olivier.lediouris@oracle.com>
2020-11-11 09:33:37 -0800

    Upgraded to ES6 (WiP)

--- serial.101.js
+++ serial.101.js
@@ -7,6 +7,31 @@
  */
 /**
  * Nov 2020, problem reading the serial ports...
+ /home/pi/node.pi/node_modules/serialport/node_modules/bindings/bindings.js:91
+ throw err
+ ^
+
+ Error: Could not locate the bindings file. Tried:
+ → /home/pi/node.pi/node_modules/serialport/build/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/build/Debug/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/build/Release/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/out/Debug/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/Debug/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/out/Release/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/Release/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/build/default/serialport.node
+ → /home/pi/node.pi/node_modules/serialport/compiled/10.23.0/linux/arm/serialport.node
+ at bindings (/home/pi/node.pi/node_modules/serialport/node_modules/bindings/bindings.js:88:9)
+ at Object.<anonymous> (/home/pi/node.pi/node_modules/serialport/lib/bindings.js:3:35)
+ at Module._compile (internal/modules/cjs/loader.js:778:30)
+ at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)
+ at Module.load (internal/modules/cjs/loader.js:653:32)
+ at tryModuleLoad (internal/modules/cjs/loader.js:593:12)
+ at Function.Module._load (internal/modules/cjs/loader.js:585:3)
+ at Module.require (internal/modules/cjs/loader.js:692:17)
+ at require (internal/modules/cjs/helpers.js:25:18)
+ at Object.<anonymous> (/home/pi/node.pi/node_modules/serialport/lib/serialport.js:12:25)
+
  */
 let SerialPort = require('serialport');
 let port = new SerialPort('/dev/ttyS80', {
